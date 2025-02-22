c4a349964a6c6b26f360827b1eb0b62a7564c78c
= <jeremy.ws.2008@gmail.com>
2025-02-21 22:52:01 -0600

    Added basic https server functionality with private keys, along with organizing main

--- email-schedule-backend/src/main.rs
+++ email-schedule-backend/src/main.rs
@@ -1,3 +1,147 @@
+//! Simple HTTPS echo service based on hyper_util and rustls
+//!
+//! First parameter is the mandatory port to use.
+//! Certificate and private key are hardcoded to sample files.
+//! hyper will automatically use HTTP/2 if a client starts talking HTTP/2,
+//! otherwise HTTP/1.1 will be used.
+
+use std::net::{Ipv4Addr, SocketAddr};
+use std::path::PathBuf;
+use std::sync::Arc;
+use std::{env, fs, io};
+
+use hyper::service::service_fn;
+use hyper_util::rt::{TokioExecutor, TokioIo};
+use hyper_util::server::conn::auto::Builder;
+use rustls::pki_types::{CertificateDer, PrivateKeyDer};
+use rustls::ServerConfig;
+use tokio::net::TcpListener;
+use tokio_rustls::TlsAcceptor;
+
+mod server;
 fn main() {
-    println!("Hello, world!");
+    // Serve an echo service over HTTPS, with proper error handling.
+    if let Err(e) = run_server() {
+        eprintln!("FAILED: {}", e);
+        std::process::exit(1);
+    }
+}
+
+fn error(err: String) -> io::Error {
+    io::Error::new(io::ErrorKind::Other, err)
+}
+
+fn get_absolute_path(relative_path: &str) -> io::Result<PathBuf> {
+    let mut path = env::current_dir()?;
+    path.push(relative_path);
+    Ok(path)
+}
+async fn shutdown_signal() {
+    // Wait for the CTRL+C signal
+    tokio::signal::ctrl_c()
+        .await
+        .expect("failed to install CTRL+C signal handler");
+}
+
+#[tokio::main]
+async fn run_server() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    // Set a process wide default crypto provider.
+    let _ = rustls::crypto::aws_lc_rs::default_provider().install_default();
+
+    // First parameter is port number (optional, defaults to 1337)
+    let port = match env::args().nth(1) {
+        Some(ref p) => p.parse()?,
+        None => 1337,
+    };
+    let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), port);
+
+    let certs_path = get_absolute_path("encrypt-files/sample.pem")?;
+    let key_path = get_absolute_path("encrypt-files/sample.rsa")?;
+
+    // Load public certificate.
+    let certs = load_certs(certs_path.to_str().unwrap())?;
+    // Load private key.
+    let key = load_private_key(key_path.to_str().unwrap())?;
+
+    println!("Starting to serve on https://{}", addr);
+
+    // Create a TCP listener via tokio.
+    let incoming = TcpListener::bind(&addr).await?;
+
+    // the graceful watcher
+    let graceful = hyper_util::server::graceful::GracefulShutdown::new();
+    // when this signal completes, start shutdown
+    let mut signal = std::pin::pin!(shutdown_signal());
+
+    // Build TLS configuration.
+    let mut server_config = ServerConfig::builder()
+        .with_no_client_auth()
+        .with_single_cert(certs, key)
+        .map_err(|e| error(e.to_string()))?;
+    server_config.alpn_protocols = vec![b"h2".to_vec(), b"http/1.1".to_vec(), b"http/1.0".to_vec()];
+    let tls_acceptor = TlsAcceptor::from(Arc::new(server_config));
+
+    let service = service_fn(server::serve);
+
+    loop {
+        tokio::select! {
+            Ok((tcp_stream, _remote_addr)) = incoming.accept() => {
+                let tls_acceptor = tls_acceptor.clone();
+            tokio::spawn(async move {
+                let tls_stream = match tls_acceptor.accept(tcp_stream).await {
+                    Ok(tls_stream) => tls_stream,
+                    Err(err) => {
+                        eprintln!("failed to perform tls handshake: {err:#}");
+                        return;
+                    }
+                };
+                if let Err(err) = Builder::new(TokioExecutor::new())
+                    .serve_connection(TokioIo::new(tls_stream), service)
+                    .await
+                {
+                    eprintln!("failed to serve connection: {err:#}");
+                }
+            });
+        }
+
+         _ = &mut signal => {
+            // shutdown signal received
+            eprintln!("Shutting down");
+            break;
+        }
+
+        }
+    }
+    tokio::select! {
+        _ = graceful.shutdown() => {
+            eprintln!("all connections gracefully closed");
+            Ok(())
+        },
+        _ = tokio::time::sleep(std::time::Duration::from_secs(10)) => {
+            eprintln!("timed out wait for all connections to close");
+            Ok(())
+        }
+    }
+}
+
+// Load public certificate from file.
+fn load_certs(filename: &str) -> io::Result<Vec<CertificateDer<'static>>> {
+    // Open certificate file.
+    let certfile = fs::File::open(filename)
+        .map_err(|e| error(format!("failed to open {}: {}", filename, e)))?;
+    let mut reader = io::BufReader::new(certfile);
+
+    // Load and return certificate.
+    rustls_pemfile::certs(&mut reader).collect()
+}
+
+// Load private key from file.
+fn load_private_key(filename: &str) -> io::Result<PrivateKeyDer<'static>> {
+    // Open keyfile.
+    let keyfile = fs::File::open(filename)
+        .map_err(|e| error(format!("failed to open {}: {}", filename, e)))?;
+    let mut reader = io::BufReader::new(keyfile);
+
+    // Load and return a single private key.
+    rustls_pemfile::private_key(&mut reader).map(|key| key.unwrap())
 }
---
+++ email-schedule-backend/src/server/mod.rs
@@ -0,0 +1,24 @@
+
+use http_body_util::{BodyExt, Full};
+use hyper::body::{Bytes, Incoming};
+use hyper::http::{Method, Request, Response, StatusCode};
+// Custom echo service, handling two different routes and a
+// catch-all 404 responder.
+pub async fn serve(req: Request<Incoming>) -> Result<Response<Full<Bytes>>, hyper::Error> {
+    let mut response = Response::new(Full::default());
+    match (req.method(), req.uri().path()) {
+        // Help route.
+        (&Method::GET, "/") => {
+            *response.body_mut() = Full::from("Try POST /echo\n");
+        }
+        // Echo service route.
+        (&Method::POST, "/echo") => {
+            *response.body_mut() = Full::from(req.into_body().collect().await?.to_bytes());
+        }
+        // Catch-all 404.
+        _ => {
+            *response.status_mut() = StatusCode::NOT_FOUND;
+        }
+    };
+    Ok(response)
+}
